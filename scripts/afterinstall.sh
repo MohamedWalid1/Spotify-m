7b92360e0cf029f4128627eb3a3afcca87177cbd
Michael <michaelsoliman981@gmail.com>
2020-06-11 19:14:40 +0200

    testing codedeploy

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+cp /tmp/.env /home/ubuntu/Spotify-m/client
 cd /home/ubuntu/Spotify-m/client
 npm install
 npm rebuild
-REACT_APP_CLIENT_URL=http://localhost:3000 npm run build:client
+npm run build:client
 cd /home/ubuntu/Spotify-m/api
 npm install
 chmod +x api/src/spotify-api.js
